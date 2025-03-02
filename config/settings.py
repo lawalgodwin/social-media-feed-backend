5bcd3ade5599e415356d27e79fdad09fe2a27ea6
lawalgodwin <godwinl200@gmail.com>
2025-03-02 14:43:15 +0100

    feat: create models for posts, comments, and interactions

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'feed',
 ]
 
 MIDDLEWARE = [
@@ -114,6 +115,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'feed.User'
 
 
 # Internationalization
