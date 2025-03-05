8f91bba7222adad16560a45d24efb0dd4a369f2c
lawalgodwin <godwinl200@gmail.com>
2025-03-05 03:55:33 +0100

    feat: implement authentication and authorization

--- feed/mutations.py
+++ feed/mutations.py
@@ -250,6 +250,6 @@ class FeedMutation(graphene.ObjectType):
 
 
 class AuthMutation(graphene.ObjectType):
-    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    sign_in = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
